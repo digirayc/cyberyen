f699f1ee9edc86eb553714f4dd290591e7e1b0dc
David Burkett <davidburkett38@gmail.com>
2026-04-30 15:06:09 -0400

    Fix cache leaf bounds check

--- src/libmw/src/mmr/PMMRCache.cpp
+++ src/libmw/src/mmr/PMMRCache.cpp
@@ -11,11 +11,11 @@ LeafIndex PMMRCache::AddLeaf(const Leaf& leaf)
     auto rightHash = leaf.GetHash();
     auto nextIdx = leaf.GetNodeIndex().GetNext();
     while (!nextIdx.IsLeaf()) {
-	const mw::Hash leftHash = GetHash(nextIdx.GetLeftChild());
-	rightHash = MMRUtil::CalcParentHash(nextIdx, leftHash, rightHash);
+        const mw::Hash leftHash = GetHash(nextIdx.GetLeftChild());
+        rightHash = MMRUtil::CalcParentHash(nextIdx, leftHash, rightHash);
 
-	m_nodes.push_back(rightHash);
-	nextIdx = nextIdx.GetNext();
+        m_nodes.push_back(rightHash);
+        nextIdx = nextIdx.GetNext();
     }
 
     m_leaves.push_back(leaf);
@@ -25,12 +25,12 @@ LeafIndex PMMRCache::AddLeaf(const Leaf& leaf)
 Leaf PMMRCache::GetLeaf(const LeafIndex& leafIdx) const
 {
     if (leafIdx < m_firstLeaf) {
-	return m_pBase->GetLeaf(leafIdx);
+        return m_pBase->GetLeaf(leafIdx);
     }
 
     const uint64_t cacheIdx = leafIdx.Get() - m_firstLeaf.Get();
-    if (cacheIdx > m_leaves.size()) {
-	throw std::out_of_range("Attempting to access non-existent leaf");
+    if (cacheIdx >= m_leaves.size()) {
+        throw std::out_of_range("Attempting to access non-existent leaf");
     }
 
     return m_leaves[cacheIdx];
@@ -39,20 +39,20 @@ Leaf PMMRCache::GetLeaf(const LeafIndex& leafIdx) const
 LeafIndex PMMRCache::GetNextLeafIdx() const noexcept
 {
     if (m_leaves.empty()) {
-	return m_firstLeaf;
+        return m_firstLeaf;
     } else {
-	return m_leaves.back().GetLeafIndex().Next();
+        return m_leaves.back().GetLeafIndex().Next();
     }
 }
 
 mw::Hash PMMRCache::GetHash(const Index& idx) const
 {
     if (idx < m_firstLeaf.GetPosition()) {
-	return m_pBase->GetHash(idx);
+        return m_pBase->GetHash(idx);
     } else {
-	const uint64_t vecIdx = idx.GetPosition() - m_firstLeaf.GetPosition();
-	assert(m_nodes.size() > vecIdx);
-	return m_nodes[vecIdx];
+        const uint64_t vecIdx = idx.GetPosition() - m_firstLeaf.GetPosition();
+        assert(m_nodes.size() > vecIdx);
+        return m_nodes[vecIdx];
     }
 }
 
@@ -62,23 +62,23 @@ void PMMRCache::Rewind(const uint64_t numLeaves)
 
     LeafIndex nextLeaf = LeafIndex::At(numLeaves);
     if (nextLeaf <= m_firstLeaf) {
-	m_firstLeaf = nextLeaf;
-	m_leaves.clear();
-	m_nodes.clear();
+        m_firstLeaf = nextLeaf;
+        m_leaves.clear();
+        m_nodes.clear();
     } else if (!m_leaves.empty()) {
-	auto iter = m_leaves.begin();
-	while (iter != m_leaves.end() && iter->GetLeafIndex() < nextLeaf) {
-	    iter++;
-	}
+        auto iter = m_leaves.begin();
+        while (iter != m_leaves.end() && iter->GetLeafIndex() < nextLeaf) {
+            iter++;
+        }
 
-	if (iter != m_leaves.end()) {
-	    m_leaves.erase(iter, m_leaves.end());
-	}
+        if (iter != m_leaves.end()) {
+            m_leaves.erase(iter, m_leaves.end());
+        }
 
-	const uint64_t numNodes = GetNumNodes() - m_firstLeaf.GetPosition();
-	if (m_nodes.size() > numNodes) {
-	    m_nodes.erase(m_nodes.begin() + numNodes, m_nodes.end());
-	}
+        const uint64_t numNodes = GetNumNodes() - m_firstLeaf.GetPosition();
+        if (m_nodes.size() > numNodes) {
+            m_nodes.erase(m_nodes.begin() + numNodes, m_nodes.end());
+        }
     }
 }
 
@@ -91,17 +91,17 @@ void PMMRCache::BatchWrite(
     LOG_TRACE_F("Writing batch {}", firstLeafIdx.Get());
     Rewind(firstLeafIdx.Get());
     for (const Leaf& leaf : leaves) {
-	Add(leaf.vec());
+        Add(leaf.vec());
     }
 }
 
 void PMMRCache::Flush(const uint32_t file_index, const std::unique_ptr<mw::DBBatch>& pBatch)
 {
     LOG_TRACE_F(
-	"Flushing {} leaves at {} with file index {}",
-	m_leaves.size(),
-	m_firstLeaf.Get(),
-	file_index
+        "Flushing {} leaves at {} with file index {}",
+        m_leaves.size(),
+        m_firstLeaf.Get(),
+        file_index
     );
     m_pBase->BatchWrite(file_index, m_firstLeaf, m_leaves, pBatch);
     m_firstLeaf = GetNextLeafIdx();
