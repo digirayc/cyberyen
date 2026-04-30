683747552e9e64e4851b8977d7a224ebbce9b65b
digirayc <kargain.org@gmail.com>
2026-04-30 13:47:56 +0300

    ci: fix manifest.scm - use specification->package for stability

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -1,52 +1,37 @@
 (use-modules (guix packages)
-             (guix download)
-             (guix build-system gnu)
-             (guix licenses)
-             (gnu packages)
-             (gnu packages autotools)
-             (gnu packages base)
-             (gnu packages boost)
-             (gnu packages commencement)     ; for gcc-toolchain
-             (gnu packages compression)
-             (gnu packages databases)
-             (gnu packages gcc)
-             (gnu packages gettext)
-             (gnu packages glib)
-             (gnu packages libevent)
-             (gnu packages pkg-config)
-             (gnu packages qt)
-             (gnu packages version-control)
-             (gnu packages xorg))
+             (guix profiles)
+             (gnu packages))
 
 (packages->manifest
- (list
-  ;; Core build tools
-  gcc-toolchain                    ; using default version (works reliably in CI)
-  make
-  autoconf
-  automake
-  libtool
-  pkg-config
+ (map specification->package
+      (list
+       ;; Core build toolchain
+       "gcc-toolchain@10"
+       "make"
+       "autoconf"
+       "automake"
+       "libtool"
+       "pkg-config"
 
-  ;; Required dependencies for Cyberyen
-  boost@1.81
-  qtbase@5
-  qttools@5
-  libevent
-  miniupnpc
-  qrencode
-  sqlite
-  zeromq
+       ;; Cyberyen dependencies (Litecoin 0.21 base)
+       "boost@1.81"
+       "qtbase@5"
+       "qttools@5"
+       "libevent"
+       "miniupnpc"
+       "qrencode"
+       "sqlite"
+       "zeromq"
 
-  ;; Optional but useful
-  berkeley-db-4
-  zlib
-  bzip2
-  xz
+       ;; Optional but recommended
+       "berkeley-db@4"
+       "zlib"
+       "bzip2"
+       "xz"
 
-  ;; Build tools
-  git
-  which
-  coreutils
-  bash
-  ))
+       ;; Build utilities
+       "git"
+       "which"
+       "coreutils"
+       "bash-minimal"
+       )))
